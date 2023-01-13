f9054e1a03e018ee9027456ba987d04fc605d8ad
Jason Duckworth <duckworthj39@gmail.com>
2023-01-13 19:52:49 +0000

    added BarChart component for displaying the website data

---
+++ src/components/BarChart.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+const BarChart = ({ labels, data, label, backgroundColor, borderColor, borderWidth }) => {
+  const chartRef = useRef(null);
+  let chart = null;
+
+  useEffect(() => {
+    if (chartRef.current) {
+      chart = new Chart(chartRef.current.getContext('2d'), {
+        type: 'bar',
+        data: {
+          labels,
+          datasets: [{
+            label,
+            data,
+            backgroundColor,
+            borderColor,
+            borderWidth
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+    return () => {
+      if(chart){
+        chart.destroy();
+      }
+    }
+  }, [labels, data, label, backgroundColor, borderColor, borderWidth]);
+
+  return <canvas id={labels} ref={chartRef} />;
+};
+
+export default BarChart;
--- src/components/WebsiteData.js
+++ src/components/WebsiteData.js
@@ -1,74 +1,57 @@
-import React, {useEffect, useState} from "react";
-import Chart from 'chart.js/auto'
+import React, { useState, useEffect } from 'react';
+import BarChart from './BarChart';
 
-function WebsiteData({url}) {
-  const [data, setData] = useState(null);
-  const [chart, setChart] = useState(null);
-
-  useEffect(() => {
-    async function fetchData(url) {
-      debugger;
-      const response = fetch(`http://localhost:3001/api/pagespeed?url=${url}`)
-        .then(res => res.json())
-        .then(data => setData(data))
-    }
-
-    fetchData(url);
-  }, [url]);
+const WebsiteCharts = () => {
+  const [chartData, setChartData] = useState([]);
+  const [amazonData, setAmazonData] = useState([]);
+  const [bestBuyData, setBestBuyData] = useState({});
+  const [targetData, setTargetData] = useState({});
+  const [walmartData, setWalmartData] = useState({});
 
   useEffect(() => {
-    if (data) {
-      const ctx = document.getElementById('chart').getContext('2d');
-      setChart(new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['First Contentful Paint', 'Speed Index', 'Time to Interactive'],
-          datasets: [{
-            label: 'Value',
-            data: [
-              data.lighthouseResult.audits['first-contentful-paint'].displayValue,
-              data.lighthouseResult.audits['speed-index'].displayValue,
-              data.lighthouseResult.audits['interactive'].displayValue
-            ],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      }));
-    }
-  }, [data]);
-
-  if (!data) {
-    return <div>Loading...</div>;
-  }
+    // Fetch data from your Express endpoint
+    debugger
+    fetch(`http://localhost:3001/api/pagespeed`)
+      .then(response => response.json())
+      .then(data => {
+        debugger;
+        // setChartData(data);
+        setAmazonData(data.find(function(item) { return item.site === 'https://www.amazon.com'; }));
+        setBestBuyData(data.find(function(item) { return item.site === 'https://www.bestbuy.com'; }));
+        setTargetData(data.find(function(item) { return item.site === 'https://www.target.com'; }));
+        setWalmartData(data.find(function(item) { return item.site === 'https://www.walmart.com'; }));
+        debugger
+      })
+      .catch(error => console.log(error));
+  }, []);
 
   return (
-    <div>
-      <canvas id="chart"></canvas>
-    </div>
+    <>
+      <BarChart
+        labels={['Amazon', 'Walmart']}
+        data={[amazonData.speedScore, walmartData.speedScore]}
+        label='Performance Walmart'
+        backgroundColor='rgba(255, 99, 132, 0.2)'
+        borderColor='rgba(255, 99, 132, 1)'
+        borderWidth={1}
+      />
+      <BarChart
+        labels={['Amazon', 'Bestbuy']}
+        data={[amazonData.speedScore, bestBuyData.speedScore]}
+        label='Performance Best Buy'
+        backgroundColor='rgba(54, 162, 235, 0.2)'
+        borderColor='rgba(54, 162, 235, 1)'
+        borderWidth={1}
+      />
+      <BarChart
+        labels={['Amazon', 'Target']}
+        data={[amazonData.speedScore, targetData.speedScore]}
+        label='Performance Target'
+        backgroundColor='rgba(255, 206, 86, 0.2)'
+        borderColor='rgba(255, 206, 86, 1)'
+        borderWidth={1}
+      />
+    </>
   );
-}
-
-export default function App() {
-  const [url, setUrl] = useState('https://amazon.com');
-
-  return (
-    <div>
-      <form>
-        <label>
-          URL:
-          <input type="text" value={url} onChange={(e) => setUrl(e.target.value)}/>
-        </label>
-      </form>
-      <WebsiteData url={url}/>
-    </div>
-  );
-}
+};
+export default WebsiteCharts;
